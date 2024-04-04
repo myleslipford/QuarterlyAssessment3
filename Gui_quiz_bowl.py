fb0be0dc187c20e789a9c2b8362364d579fad864
myleslipford <myles.lipford12@gmail.com>
2024-04-04 06:37:27 -0500

    created a quiz window class for next page

--- Gui_quiz_bowl.py
+++ Gui_quiz_bowl.py
@@ -22,8 +22,27 @@ class QuizBowlGame:
 
             start_button = Button(self.master, text="Start Quiz Now", command=self.start_quiz)
             start_button.grid(row=1, columnspan=2, padx=10, pady=10)
-            
+
     def start_quiz(self):
         category = self.category_var.get()
         self.master.destroy()
         QuizWindow(category)
+
+
+class QuizWindow:
+    def __init__(self, category):
+        self.category = category
+
+        self.questions = self.load_questions_from_db()
+        self.total_questions = len(self.questions)
+        self.correct_answers = 0
+
+        self.root = Tk()
+        self.root.title("Quiz Window")
+
+        self.current_question_index = 0
+
+        self.selected_option = StringVar()
+
+        self.create_widgets()
+        self.display_question()  # Call display_question after creating all widgets
