423246cc70ea12ffc0efadccfa7d12081369aa39
myleslipford <myles.lipford12@gmail.com>
2024-04-04 06:09:32 -0500

    create tables for different categories

--- Create_QandA_database.py
+++ Create_QandA_database.py
@@ -4,3 +4,11 @@ import sqlite3
 conn = sqlite3.connect('QuestionsDatabase.db')
 cursor = conn.cursor()
 
+# Create tables for different categories
+categories = [
+    'Business Statistics',
+    'Business Database mgmt Science',
+    'General Biology II',
+    'Mgmt organization Behavior',
+    'Business Applications'
+]
